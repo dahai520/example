63ea26de61820791a8df6af2ee42aad1d5b9ac4b
example <example@example.com>
2021-10-31 11:48:23 +0800

    refactor: refactor all project

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# demo
-
-
-[![My CI](https://github.com/dahai520/example/actions/workflows/my-ci.yml/badge.svg)](https://github.com/dahai520/example/actions/workflows/my-ci.yml)
+# Example
--- src/server/sql.ts
+++ src/server/sql.ts
@@ -1,9 +1,47 @@
-export function render(): void {
-  const div: HTMLDivElement = document.createElement('div');
-  div.innerHTML = `
-    <div style="color:#888888">
-      <h1>Hello World</h1>
-    </div>
-  `;
-  document.querySelector('body').appendChild(div);
+import mysql from 'mysql';
+
+function createPool(config: any) {
+  return function doPool(sql: string) {
+    return new Promise((resolve, reject) => {
+      const pool = mysql.createPool({
+        host : config.host,
+        user : config.user,
+        password : config.password,
+      });
+      pool.query(sql, (error, results, fields) => {
+        if (error) {
+          pool.end();
+          reject(error);
+        } else {
+          pool.end();
+          resolve(results);
+        }
+      });
+    });
+  }
+}
+
+function createConnect(config: any) {
+  return function doConnect(sql:string, values?: any) {
+    const conn = mysql.createConnection(config)
+    conn.connect();
+    return new Promise((resolve, reject) => {
+      conn.query(sql, values, (err: any, rows: any) => {
+        if (err) {
+          conn.end();
+          reject(err);
+        } else {
+          conn.end();
+          resolve(rows);
+        }
+      })
+    });
+  }
+}
+
+
+
+export {
+  createPool,
+  createConnect,
 }
